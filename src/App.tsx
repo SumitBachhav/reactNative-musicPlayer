e17d5220ced46b4a50cd69a7907b9330d6283e8c
SumitBachhav <sumitb2156@gmail.com>
2024-02-05 06:23:38 +0530

    installing react native track player, creating assets file, moving app.tsx into src folder

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+
+
+function App(): React.JSX.Element {
+
+
+  return (
+    <SafeAreaView>
+      <StatusBar
+      />
+
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
